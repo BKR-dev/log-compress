64f1b9e05ded9a55b8fd0995c6f2a1f701cbbcf8
BKR-dev <giga@sigmachad.quest>
2023-08-15 14:31:24 +0200

    only look a little bit, wip

--- main.go
+++ main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"runtime"
 	"time"
-
-	pgzip "github.com/klauspost/pgzip"
 )
 
 func main() {
+	// 1024 * 1024 * 1024 (1024^3)
+	// = 1 Gigglybitse
+	partSize := 1_073_741_824
 
 	PrintMemUsage()
 	// displayReadBytes()
@@ -18,81 +18,97 @@ func main() {
 	fmt.Printf("Starting at: %s\n", now)
 	filename := "test.log"
 	compFilename := "test.gz"
+
+	partCount, _, _ := partInfo(filename, partSize)
+	fmt.Println("Partcount: ", partCount)
+	duration := time.Since(now)
+	fmt.Printf("Finished in: %s\n", duration)
+	os.Exit(1)
+
 	decompressFilename := "decompressed.test"
-	compressFile(filename, compFilename)
+	// compressFile(filename, compFilename)
 	fmt.Println("Finished compression")
 	// 30 475 158
 	decompressFile(compFilename, decompressFilename)
-	duration := time.Since(now)
-	fmt.Printf("Finished in: %s\n", duration)
 	PrintMemUsage()
 }
 
-func compressFile(filename, compFilename string) {
-
-	// 1024 * 1024 * 1024 (1024^3)
-	// = 1 Gigglybitse
-	partSize := 1_073_741_824
-
-	// buffer size of 1G
-	buf1G := make([]byte, partSize)
-
+func fileInformation(filename string) (int, error) {
 	// open log file
-	file, _ := os.Open(filename)
+	file, err := os.Open(filename)
 	defer file.Close()
-	// create compressed file
-	compFile, err := os.Create(compFilename)
 	if err != nil {
-		fmt.Println(err)
+		return 0, err
 	}
-	defer compFile.Close()
-
 	// get filesize
-	fI, _ := os.Stat(filename)
-	fileSize := int(fI.Size())
-	// read "first" 1G into buffer
-	d1Size, _ := io.ReadFull(file, buf1G)
-	// subtract copied bytes from filesize
-	fileSize -= d1Size
-	// making it easy for me to keep track
-	fmt.Printf("fileSize: %d\npartSize: %d\nbufSize: %d\n", fileSize, d1Size, len(buf1G))
-
-	PrintMemUsage()
-
-	gWriter := pgzip.NewWriter(compFile)
-	defer gWriter.Close()
-
-	_, err = gWriter.Write(buf1G)
+	fileInfo, err := os.Stat(filename)
 	if err != nil {
-		fmt.Println(err)
+		return 0, err
 	}
-
-	_, _ = file.Seek(int64(partSize), 0)
-
-	d1Size, _ = io.ReadFull(file, buf1G)
-	// subtract copied bytes from filesize
-	fileSize -= d1Size
-	// making it easy for me to keep track
-	fmt.Printf("fileSize: %d\npartSize: %d\nbufSize: %d\n", fileSize, d1Size, len(buf1G))
-
-	PrintMemUsage()
-
-	compFilename = "test2.gz"
-
-	compFile, err = os.Create(compFilename)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer compFile.Close()
-	gWriter = pgzip.NewWriter(compFile)
-	defer gWriter.Close()
-
-	_, err = gWriter.Write(buf1G)
-	if err != nil {
-		fmt.Println(err)
-	}
-
+	return int(fileInfo.Size()), nil
 }
+
+func partInfo(filename string, partSize int) (int, int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer file.Close()
+	// get filesize
+	fileInfo, err := os.Stat(filename)
+	if err != nil {
+		return 0, 0, err
+	}
+	fileSize := int(fileInfo.Size())
+
+	partCount := fileSize / partSize
+	lastPartSize := fileSize % partSize
+	fmt.Printf("Dividing fileSize %d by partSize %d: %d\n", fileSize, partSize, partCount)
+	fmt.Println("Last Part is of Size ", lastPartSize)
+
+	return partCount, lastPartSize, nil
+}
+
+// func compressFile(filename, compFilename string) {
+
+// 	// buffer size of 1G
+// 	buf1G := make([]byte, partSize)
+
+// 	// create compressed file
+// 	compFile, err := os.Create(compFilename)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer compFile.Close()
+
+// 	// read "first" 1G into buffer
+// 	d1Size, _ := io.ReadFull(file, buf1G)
+// 	// subtract copied bytes from filesize
+// 	fileSize -= d1Size
+// 	// making it easy for me to keep track
+// 	// fmt.Printf("fileSize: %d\npartSize: %d\nbufSize: %d\n", fileSize, d1Size, len(buf1G))
+
+// 	PrintMemUsage()
+
+// 	gWriter := pgzip.NewWriter(compFile)
+// 	defer gWriter.Close()
+
+// 	_, err = gWriter.Write(buf1G)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+
+// 	// _, _ = file.Seek(int64(partSize), 0)
+
+// 	// d1Size, _ = io.ReadFull(file, buf1G)
+// 	// subtract copied bytes from filesize
+// 	// fileSize -= d1Size
+// 	// making it easy for me to keep track
+// 	// fmt.Printf("fileSize: %d\npartSize: %d\nbufSize: %d\n", fileSize, d1Size, len(buf1G))
+
+// 	PrintMemUsage()
+
+// }
 
 func PrintMemUsage() {
 	var m runtime.MemStats
