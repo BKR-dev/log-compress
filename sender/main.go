b0bb3f9cac8a31a08524a46589f2c287bf786331
Lionel <nope@not.today>
2023-08-29 08:34:49 +0200

    working on stdout

--- sender/main.go
+++ sender/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
 func main() {
+	dankStuffFoundOnline()
+	// readFromStdOut()
+
+}
+
+func readFromStdOut() []byte {
+	buf := make([]byte, 1024)
+	os.Stdout.Seek(1024, 2)
+	_, err := io.ReadFull(os.Stdout, buf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(buf)
+	return buf
+}
+
+func dankStuffFoundOnline() {
+	oldOut := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	outC := make(chan string)
+
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	w.Close()
+	os.Stdout = oldOut
+	out := <-outC
+	fmt.Println(out)
 
 }
